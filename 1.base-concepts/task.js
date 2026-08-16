519d5bf72a4fef4094c2f624c789d2985b5ecc5e
pashtet201807-ops <pashtet201807@gmail.com>
2026-08-16 11:29:14 +0300

    Implement calculateTotalMortgage function
    Added calculateTotalMortgage function to compute total mortgage amount.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -16,3 +16,25 @@ function solveEquation(a, b, c) {
   let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
   return [root1, root2];
 }
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let parsedPercent = Number(percent);
+  let parsedContribution = Number(contribution);
+  let parsedAmount = Number(amount);
+  let parsedCountMonths = Number(countMonths);
+
+  if (Number.isNaN(parsedPercent) || Number.isNaN(parsedContribution) || 
+      Number.isNaN(parsedAmount) || Number.isNaN(parsedCountMonths)) {
+    return false;
+  }
+
+  let p = parsedPercent / 100 / 12;
+  let s = parsedAmount - parsedContribution;
+  let n = parsedCountMonths;
+
+  let monthlyPayment = s * (p + (p / (((1 + p) ** n) - 1)));
+  let totalAmount = monthlyPayment * n;
+
+  return Number(totalAmount.toFixed(2));
+}
