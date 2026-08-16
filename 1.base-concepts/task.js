acf0b1792b8e8ab34284227464e35eab5b25db1b
pashtet201807-ops <pashtet201807@gmail.com>
2026-08-16 11:14:09 +0300

    Add solveEquation function for quadratic equations
    Implement quadratic equation solver with discriminant check.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+function solveEquation(a, b, c) {
+  let discriminant = b**2 - 4 * a * c;
   
+  if (discriminant < 0) {
+    return [];
+  }
+  
+  if (discriminant === 0) {
+    let root = -b / (2 * a);
+    return [root];
+  }
+  
+  let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+  let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  return [root1, root2];
 }
