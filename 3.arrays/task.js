174d8d946f71e6690af8810c5a9339954ecb418d
pashtet201807-ops <pashtet201807@gmail.com>
2026-09-02 11:20:38 +0300

    Enhance array comparison and user age calculation
    Added length check for array comparison and improved age range calculation.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredAges = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age);
+
+  if (filteredAges.length === 0) {
+    return 0;
+  }
+
+  const sumAges = filteredAges.reduce((acc, age) => acc + age, 0);
+  return sumAges / filteredAges.length;
 }
